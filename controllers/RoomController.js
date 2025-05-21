2b7d480990c2bfb24b83dddb0cd69c84044f438c
Kabo003 <gabrieltm003@gmail.com>
2025-05-20 20:56:47 -0500

    (FIX) ajuste del funcionamiento de los añadidos (decorators)

--- controllers/RoomController.js
+++ controllers/RoomController.js
@@ -1,12 +1,11 @@
-const room = require('../services/RoomSingleton');
-const getDecoratorByName = require('../utils/DecoratorFactory');
+const room = require('../Room/RoomSingleton');
+const SillaDecorator = require('../decorators/SillaDecorator');
+const MesaDecorator = require('../decorators/MesaDecorator');
 
-//get
 const getRoomItems = (req, res) => {
   res.json(room.getItems());
 };
 
-//post
 const addRoomItem = (req, res) => {
   const { name, imageUrl, posX, posY } = req.body;
 
@@ -14,24 +13,36 @@ const addRoomItem = (req, res) => {
     return res.status(400).json({ error: 'Faltan campos obligatorios.' });
   }
 
-  const Decorator = getDecoratorByName(name);
-  if (!Decorator) {
-    return res.status(400).json({ error: 'Tipo de ítem no reconocido.' });
+  const item = { name, imageUrl, posX, posY };
+
+  let Decorator = null;
+
+  switch (name.toLowerCase()) {
+    case 'silla':
+      Decorator = SillaDecorator;
+      break;
+    case 'mesa':
+      Decorator = MesaDecorator;
+      break;
+    default:
+      return res.status(400).json({ error: 'Tipo de ítem no reconocido.' });
   }
 
-  const item = { name, imageUrl, posX, posY };
   room.equipItem(Decorator, item);
 
   res.status(201).json({
-    message: 'Item decorado con éxito.',
+    message: 'Item agregado con éxito.',
     items: room.getItems()
   });
 };
 
-//delete
 const resetRoom = (req, res) => {
   room.clearItems();
   res.json({ message: 'Habitación reiniciada.' });
 };
 
-module.exports = { getRoomItems, addRoomItem, resetRoom };
+module.exports = {
+  getRoomItems,
+  addRoomItem,
+  resetRoom
+};
